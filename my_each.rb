9b1bcd6239ae864df8332bd0808285e4aae67978
Celina Jackson <celinanjackson@gmail.com>
2020-06-28 22:35:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,17 +8,3 @@ def my_each(array)
   end
 end
 
-my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
-end
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
